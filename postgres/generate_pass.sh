b1daa6402c131fc4682e3bbd109640394066bc49
root <root@ostk.qa.webex.com>
2019-04-03 14:36:49 +0800

    Support mysql, postgres and mongodb

---
+++ postgres/generate_pass.sh
@@ -0,0 +1,6 @@
+openssl rand -base64 6
+
+# or, if you do not want special char as the ending
+# just try:
+#   openssl rand -hex 6
+
---
+++ postgres/start.sh
@@ -0,0 +1,2 @@
+docker run --name db -d -e POSTGRES_PASSWORD=3p2sBOpz -d postgres:alpine
+
